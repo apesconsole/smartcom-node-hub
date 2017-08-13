8f44afd337630ae46cccb03ed0559ba15952d87c
apesconsole <apesconsole@gmail.com>
2017-08-12 23:12:27 -0500

    Admin added

--- app/models/config.js
+++ app/models/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'mongoSession'				: process.env.MONGODB_SESSION_URL 	|| 'mongodb://admin:admin@ds153521.mlab.com:53521/smartcom_session',
-    'authentication'			: process.env.MONGODB_USR 			|| 'mongodb://admin:admin@ds113630.mlab.com:13630/smartcom_user',
-	'cnstrntdatabase'			: process.env.MONGODB_CNSTRNT_DATA 	|| 'mongodb://admin:admin@ds013192.mlab.com:13192/construction'
+	'mongoSession'				: process.env.MONGODB_SESSION_URL 	|| '',
+    'authentication'			: process.env.MONGODB_USR 			|| '',
+	'cnstrntdatabase'			: process.env.MONGODB_CNSTRNT_DATA 	|| ''
 };
